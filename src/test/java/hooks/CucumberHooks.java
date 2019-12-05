abfd808e7570243c186c737761cd6a4714e2765f
iakovsidorov <ziforigin@gmail.com>
2019-12-05 16:42:38 +0300

    Cucumber hook for teardown added

---
+++ src/test/java/hooks/CucumberHooks.java
@@ -0,0 +1,17 @@
+package hooks;
+
+import cucumber.api.java.After;
+import utils.TestContext;
+
+public class CucumberHooks {
+    TestContext testContext;
+
+    public CucumberHooks(TestContext context) {
+        testContext = context;
+    }
+
+    @After
+    public void tearDown() {
+        testContext.getWebDriverManager().quitWebDriver();
+    }
+}
